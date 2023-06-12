5a82d7024d98eecf957d64d9cd98c5d2c191eae5
Manassés Ribeiro <manasses.ribeiro@ifc.edu.br>
2023-06-12 09:34:57 -0300

    aula 12/06/2023

--- BCC/Algoritmos(CCC0701)/C/Registro/exemplo_registro_vetor_modularizado.c
+++ BCC/Algoritmos(CCC0701)/C/Registro/exemplo_registro_vetor_modularizado.c
@@ -11,11 +11,70 @@ typedef struct{
 //Prototipação
 void carregaProdutos(Produto[], int);
 void escreveProdutos(Produto[], int);
+void informaBaixa(Produto[], int, int);
+void escreverBaixasProdutos(Produto[], int);
+void inicializaVetorBaixas(Produto[], int);
 
 int main(){
     Produto produtos[TAM];
     carregaProdutos(produtos, TAM);
     escreveProdutos(produtos, TAM);
+    informaBaixa(produtos, 0, 1);
+    informaBaixa(produtos, 0, 4);
+    informaBaixa(produtos, 0, 3);
+    informaBaixa(produtos, 0, 1);
+    informaBaixa(produtos, 0, 1);
+
+    informaBaixa(produtos, 1, 1);
+    informaBaixa(produtos, 1, 6);
+
+    escreverBaixasProdutos(produtos, TAM);
+}
+
+// Módulo que inicializa vetor baixas
+void inicializaVetorBaixas(Produto produtos[], int prod){
+    int i;
+    for (i=0; i<7;i++){
+        produtos[prod].baixa[i] = 0;
+    }
+}
+
+// Módulo informa baixas
+void informaBaixa(Produto produtos[], int codigo, int dia){
+    produtos[codigo].baixa[dia]++;
+}
+
+// Módulo escreve baixas
+void escreverBaixasProdutos(Produto produtos[], int tam){
+    int p, d;
+    for (p=0; p<tam; p++){
+        printf("\n\nProduto %s(codigo: %i):\n", produtos[p].descricao, produtos[p].codigo);
+        for (d=0; d<7; d++){
+            switch(d){
+                case 0:
+                    printf("\tDomingo: %i\n", produtos[p].baixa[d]);
+                    break;
+                case 1:
+                    printf("\tSegunda: %i\n", produtos[p].baixa[d]);
+                    break;
+                case 2:
+                    printf("\tTerca: %i\n", produtos[p].baixa[d]);
+                    break;
+                case 3:
+                    printf("\tQuarta: %i\n", produtos[p].baixa[d]);
+                    break;
+                case 4:
+                    printf("\tQuinta: %i\n", produtos[p].baixa[d]);
+                    break;
+                case 5:
+                    printf("\tSexta: %i\n", produtos[p].baixa[d]);
+                    break;
+                case 6:
+                    printf("\tSabado: %i\n", produtos[p].baixa[d]);
+                    break;
+            }
+        }
+    }
 }
 
 // Módulo carrega Produtos
@@ -23,13 +82,15 @@ void carregaProdutos(Produto produtos[], int tam){
     int i;
     // Carrega o vetor de produtos
     for (i=0; i<tam; i++){
-        printf("Informe o codigo do produto: ");
-        scanf("%i", &produtos[i].codigo);
+        //printf("Informe o codigo do produto: ");
+//        scanf("%i", &produtos[i].codigo);
         getc(stdin);
+        produtos[i].codigo = i;
         printf("Informe a descricao do produto: ");
         scanf("%[^\n]", produtos[i].descricao);
         printf("Informe o preço do produto: ");
-        scanf("%f", &produtos[i].preco);   
+        scanf("%f", &produtos[i].preco);
+        inicializaVetorBaixas(produtos, i);  
     }
 }
 
